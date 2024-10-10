88ada46d70a34853e336762e822e46e1de5ed9b2
qwqAutomaton <xuhaosen2006@163.com>
2024-10-10 16:24:39 +0800

    Memory check

--- src/loader.s
+++ src/loader.s
@@ -16,8 +16,16 @@ GDT_SIZ equ $ - GDT_BASE ; 计算当前 GDT 大小
 GDT_LIM equ GDT_SIZ - 1  ; GDT 界限
 times 60 dq 0 ; 填充 60 个空描述符
 
+; total mem: gdt(8) + code desc(8) + data desc(8) + video desc(8) + empty desc(8 * 60 = 480) = 512B
+
 gdt_ptr dw GDT_LIM
         dq GDT_BASE
+
+; additional mem for ards(20B): gdt_ptr(6) + ards_cnt(2, unsigned short) + ards(remaining 504 for 25 ARDS) = 512B.
+ards_cnt dw 0
+ards times 504 db 0
+; _TMP_STR_BUF times 16 db "$"
+
 ;--- selector ---;
 ; 代码段的选择子
 SEL_CODE equ (0x01 << 3) + SEL_TI_G + SEL_RPL0
@@ -50,6 +58,52 @@ loader_start:
     in al, dx ; 读入原来的值
     or al, 0x02 ; 第 1 位设为 1
     out dx, al ; 写入
+    ; 检查内存
+    mov ax, 0x0300
+    mov bx, 0x0000
+    int 0x10
+    mov ax, str14_
+    mov bp, ax
+    mov ax, 0x1301
+    mov bx, 0x000f
+    mov cx, 0x1c
+    int 0x10
+    xor ebx, ebx
+    mov edx, 0x534d4150
+    mov di, ards
+.check_mem:
+    mov eax, 0x0000_e820
+    mov ecx, 20
+    int 0x15
+    jc .check_mem_fail
+    inc word [ards_cnt]
+    add di, cx
+    cmp ebx, 0
+    jnz .check_mem
+    jmp .check_mem_finished
+.check_mem_fail:
+    mov ax, 0x0300
+    mov bx, 0x0000
+    int 0x10
+    mov ax, str15
+    mov bp, ax
+    mov ax, 0x1301
+    mov bx, 0x000f
+    mov cx, 0x20
+    int 0x10
+.check_mem_finished:
+    mov ax, 0x0300
+    mov bx, 0x0000
+    int 0x10
+    mov ax, str16
+    mov bp, ax
+    mov ax, 0x1301
+    mov bx, 0x000f
+    mov cx, 0x22
+    int 0x10
+    ; 打印总内存
+    ; mov eax, [ards_cnt]
+    ; call print_number
     ; 加载 GDT，设置 GDTR
     mov ax, 0x0300
     mov bx, 0x0000
@@ -76,6 +130,7 @@ loader_start:
     mov cr0, eax
     jmp dword SEL_CODE:pe_start
 
+; protected mode, embark!
 [bits 32]
 pe_start:
     mov ax, SEL_DATA
@@ -84,31 +139,68 @@ pe_start:
     mov esp, LOADER_ADDR
     mov ax, SEL_VDEO
     mov gs, ax
-    mov dword [gs:0x500], 0x24f0251
-    mov dword [gs:0x504], 0x25b0253
-    mov dword [gs:0x508], 0x24c024b
-    mov dword [gs:0x50c], 0x25d0220
-    mov dword [gs:0x510], 0x220023a
-    mov dword [gs:0x514], 0x2630241
-    mov dword [gs:0x518], 0x2690274
-    mov dword [gs:0x51c], 0x2610276
-    mov dword [gs:0x520], 0x2650274
-    mov dword [gs:0x524], 0x2200264
-    mov dword [gs:0x528], 0x2720250
-    mov dword [gs:0x52c], 0x274026f
-    mov dword [gs:0x530], 0x2630265
-    mov dword [gs:0x534], 0x2650274
-    mov dword [gs:0x538], 0x2200264
-    mov dword [gs:0x53c], 0x26e0245
-    mov dword [gs:0x540], 0x2690276
-    mov dword [gs:0x544], 0x26f0272
-    mov dword [gs:0x548], 0x26d026e
-    mov dword [gs:0x54c], 0x26e0265
-    mov dword [gs:0x550], 0x22e0274
+    mov dword [gs:0x640], 0x24f0251
+    mov dword [gs:0x644], 0x25b0253
+    mov dword [gs:0x648], 0x24c024b
+    mov dword [gs:0x64c], 0x25d0220
+    mov dword [gs:0x650], 0x220023a
+    mov dword [gs:0x654], 0x2720250
+    mov dword [gs:0x658], 0x274026f
+    mov dword [gs:0x65c], 0x2630265
+    mov dword [gs:0x660], 0x2650274
+    mov dword [gs:0x664], 0x2200264
+    mov dword [gs:0x668], 0x26f024d
+    mov dword [gs:0x66c], 0x2650264
+    mov dword [gs:0x670], 0x2610220
+    mov dword [gs:0x674], 0x2740263
+    mov dword [gs:0x678], 0x2760269
+    mov dword [gs:0x67c], 0x2740261
+    mov dword [gs:0x680], 0x2640265
+    mov dword [gs:0x684], 0x22e
+
     jmp $
 
+; ; print decimal number
+; ; number: ax
+; print_number:
+;     mov di, _TMP_STR_BUF
+;     add di, 15
+;     mov byte [di], 48
+;     ; ax = 0?
+;     mov cx, 1
+;     cmp ax, 0
+;     mov bp, di
+;     jnz .print_zero
+;     mov cx, 10
+; .decomp_loop:
+;     div cx
+;     add dx, 48
+;     mov [di], dx
+;     dec di
+;     cmp ax, 0
+;     jnz .decomp_loop
+;     mov bp, _TMP_STR_BUF
+;     mov cx, 16
+;     cmp bp, di
+;     jz .print_zero
+; .count_loop:
+;     inc bp
+;     dec cx
+;     cmp bp, di
+;     jnz .count_loop
+; .print_zero:
+;     mov ax, 0x0300
+;     mov bx, 0x0000
+;     int 0x10
+;     mov ax, 0x1301
+;     mov bx, 0x000f
+;     int 0x10
+;     ret
 
     str11 db "QOS[KL ]: KL running.", 0x0d, 0x0a, "$"
     str12 db "QOS[KL ]: Enabling A20 Gate.", 0x0d, 0x0a, "$"
     str13 db "QOS[KL ]: Loading global descriptor table.", 0x0d, 0x0a, "$"
     str14 db "QOS[KL ]: Entering protected mode.", 0x0d, 0x0a, "$"
+    str14_ db "QOS[KL ]: Checking memory.", 0x0d, 0x0a, "$"
+    str15 db "QOS[KL ]: Memory check failed.", 0x0d, 0x0a, "$"
+    str16 db "QOS[KL ]: Memory check finished.", 0x0d, 0x0a, "$"
